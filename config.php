284012b952ff73deead7c8356f9f9d6669941dfe
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-15 00:51:15 +0800

    create config.php and createDatabase.php

--- config.php
+++ config.php
@@ -3,4 +3,6 @@ $mysqli = new mysqli("localhost", "root", "", "GameReviewWebsite");
 if ($mysqli -> connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
     exit();
+} else {
+    echo "Connection to database established";
 }
