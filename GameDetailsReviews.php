9ace9c9b682bdc48ff618a06f2ad255cb90157d8
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-15 18:30:21 +0800

    Adding Header.php to GameDetailsReviews

--- GameDetailsReviews.php
+++ GameDetailsReviews.php
@@ -5,6 +5,8 @@
   <title>Page Title</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include 'header.php';
+  ?>
   <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
