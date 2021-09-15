ee29a4a4b5523e3c67880e465071653fb58a6382
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-15 15:04:11 +0800

    sql query name update

--- createTable.php
+++ createTable.php
@@ -4,11 +4,11 @@ $mysqli = new mysqli("localhost", "root", "", "GameReviewWebsite");
 if ($mysqli -> connect_errno) {
     die("Connection failed: " . $mysqli->connect_error);
 }
-$sql = "CREATE TABLE IF NOT EXISTS Users (
+$sqlUsers = "CREATE TABLE IF NOT EXISTS Users (
 user_id INT AUTO_INCREMENT PRIMARY KEY
 )";
 
-$sql1 = "CREATE TABLE IF NOT EXISTS Reviews (
+$sqlReviews = "CREATE TABLE IF NOT EXISTS Reviews (
 review_id INT(6) AUTO_INCREMENT,
 user_id INT,
 user_review VARCHAR(256) NOT NULL,
@@ -19,13 +19,13 @@ CONSTRAINT FK_user_id FOREIGN KEY (user_id)
 REFERENCES Users(user_id)
 )";
 
-if ($mysqli->query($sql) === true) {
+if ($mysqli->query($sqlUsers) === true) {
     echo "Users table created successfully";
 } else {
     echo "Error creating table: " . $mysqli->error;
 }
 
-if ($mysqli->query($sql1) === true) {
+if ($mysqli->query($sqlReviews) === true) {
     echo "Reviews table created successfully";
 } else {
     echo "Error creating table: " . $mysqli->error;
