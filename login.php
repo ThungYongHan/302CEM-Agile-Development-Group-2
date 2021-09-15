923e9afee43a3245f82be68c8e44b3a945b1b3f4
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-16 02:00:12 +0800

    Update login.php with login function

--- login.php
+++ login.php
@@ -1,2 +1,25 @@
 <?php
-echo "login.php test";
+    session_start();
+    $db = mysqli_connect('localhost', 'root', '', 'GameReviewWebsite');
+    
+    if (isset($_POST['login_user'])) {
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        if (empty($username)) {
+            echo "Username field cannot be empty!";
+        }
+        if (empty($password)) {
+            echo "Password field cannot be empty!";
+        }
+
+        $query = "SELECT * FROM Users WHERE username='$username' AND user_pass='$password'";
+        $results = mysqli_query($db, $query);
+            
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            header('Location: http://localhost:8080\AgileDev\GameBrowsingHomepage.php');
+        } else {
+            echo "Invalid username and password combination!";
+        }
+    }
