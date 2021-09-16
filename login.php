dcf54d4412a833eea62ae3a1eb4de5c5ccd8732c
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-16 15:47:52 +0800

    Update login.php with redirect back to loginsignupmodal

--- login.php
+++ login.php
@@ -1,21 +1,43 @@
 <?php
     session_start();
     $db = mysqli_connect('localhost', 'root', '', 'GameReviewWebsite');
-    
-    if (isset($_POST['login_user'])) {
-        $username = mysqli_real_escape_string($db, $_POST['username']);
-        $password = mysqli_real_escape_string($db, $_POST['password']);
 
-        if (empty($username)) {
-            echo "Username field cannot be empty!";
-        }
-        if (empty($password)) {
-            echo "Password field cannot be empty!";
-        }
+function emptyUsernameAlert()
+{
+    echo "
+    <script>
+         window.alert('Username field cannot be empty!');
+         window.location.href='LoginSignUpModal.php';
+    </script>
+    ";
+}
 
+function emptyPasswordAlert()
+{
+    echo "
+    <script>
+        window.alert('Password field cannot be empty!');
+        window.location.href='LoginSignUpModal.php';
+    </script>
+    ";
+}
+
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (empty($username)) {
+        emptyUsernameAlert();
+        // echo "Username field cannot be empty!";
+    }
+    if (empty($password)) {
+        emptyPasswordAlert();
+        // echo "Password field cannot be empty!";
+    }
+
+    if ((!empty($username)) && (!empty($password))) {
         $query = "SELECT * FROM Users WHERE username='$username' AND user_pass='$password'";
         $results = mysqli_query($db, $query);
-            
         if (mysqli_num_rows($results) == 1) {
             $_SESSION['username'] = $username;
             header('Location: http://localhost:8080\AgileDev\GameBrowsingHomepage.php');
@@ -23,3 +45,4 @@
             echo "Invalid username and password combination!";
         }
     }
+}
