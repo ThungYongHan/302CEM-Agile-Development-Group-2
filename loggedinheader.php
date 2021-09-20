6a395aa602ea57048da287eaefc422bdfb8422f2
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-20 23:01:58 +0800

    Update loggedinheader.php with homepage link according to new header

--- loggedinheader.php
+++ loggedinheader.php
@@ -28,7 +28,7 @@ function LogoutFunction() {
 
     <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #c0d3fc;">
         <div class="container">
-          <a class="navbar-brand text-dark">Game Review Website</a>
+          <a href="GameBrowsingHomepage.php" a class="navbar-brand text-dark">Game Review Website</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -42,6 +42,8 @@ function LogoutFunction() {
                 </div>
                    <?php if (isset($_SESSION['username'])) : ?>
                     <a class="navbar-brand text-dark">Welcome, <?php echo $_SESSION['username']; ?></a>
+                    <!-- <button type="button" button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button> -->
+
                      <button class="btn btn-secondary" button type="button" onclick="LogoutFunction()">LogOut</button>
                   <?php endif ?>
             </form>
