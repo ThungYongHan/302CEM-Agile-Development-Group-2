4ccf68d537424e0a4ce8e041ae7a45e032c95ed6
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-09-20 22:59:09 +0800

    Added correct username wrong password validation to login.php

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
-    session_start();
-    $db = mysqli_connect('localhost', 'root', '', 'GameReviewWebsite');
+session_start();
 
+$db = mysqli_connect('localhost', 'root', '', 'GameReviewWebsite');
 function emptyUsernameAlert()
 {
     echo
     "
     <script>
          window.alert('Username field cannot be empty!');
-         window.location.href='LoginSignUpModal.php';
+         window.location.href='GameBrowsingHomepage.php';
     </script>
     ";
 }
@@ -19,7 +19,7 @@ function emptyPasswordAlert()
     "
     <script>
         window.alert('Password field cannot be empty!');
-        window.location.href='LoginSignUpModal.php';
+        window.location.href='GameBrowsingHomepage.php';
     </script>
     ";
 }
@@ -30,10 +30,22 @@ function invalidLoginAlert()
     "
     <script>
         window.alert('Invalid username and password combination!');
-        window.location.href='LoginSignUpModal.php';
+        window.location.href='GameBrowsingHomepage.php';
     </script>
     ";
 }
+
+function invalidPasswordAlert()
+{
+    echo
+    "
+    <script>
+        window.alert('Username is found in database but the password is incorrect!');
+        window.location.href='GameBrowsingHomepage.php';
+    </script>
+    ";
+}
+
 
 if (isset($_POST['login_user'])) {
     $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -48,8 +60,14 @@ if (isset($_POST['login_user'])) {
 
     if ((!empty($username)) && (!empty($password))) {
         $query = "SELECT * FROM Users WHERE username='$username' AND user_pass='$password'";
+        $usernamequery = "SELECT * FROM Users WHERE username='$username'";
+
         $results = mysqli_query($db, $query);
-        if (mysqli_num_rows($results) == 1) {
+        $usernameresults = mysqli_query($db, $usernamequery);
+        
+        if (mysqli_num_rows($usernameresults) == 1 && mysqli_num_rows($results) != 1) {
+            invalidPasswordAlert();
+        } elseif (mysqli_num_rows($results) == 1) {
             $_SESSION['username'] = $username;
             header('Location: http://localhost:8080\AgileDev\GameBrowsingHomepage.php');
         } else {
