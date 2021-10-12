8fef44ac792e1607bba0fcbb89d12c50b03a37b7
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-10-13 01:23:54 +0800

    Code cleanup for addGameDetails

--- addGameDetails.php
+++ addGameDetails.php
@@ -17,18 +17,18 @@ $game_year = $_POST["gameyear"];
         $problem = false;
         $use = $_SESSION['username'];
 
-        $extension = substr($game_cover, strlen($game_cover)-4, strlen($game_cover));
-        $allowed_extensions = array(".jpg", ".png", ".jpeg");
-        if (!in_array($extension, $allowed_extensions)) {
-            echo "<script type='text/javascript'>
-                    alert ('Invalid format for the image. Only jpg / jpeg / png format allowed.');
-                </script>";
-            $problem = true;
-        } else {
-            //$game_img = md5($game_cover).$extension;
-            //move_uploaded_file($_FILES["gamecover"]["tmp_name"], "uploadedImg/".$game_img);
-            $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
-        }
+        // $extension = substr($game_cover, strlen($game_cover)-4, strlen($game_cover));
+        // $allowed_extensions = array(".jpg", ".png", ".jpeg");
+        // if (!in_array($extension, $allowed_extensions)) {
+        //     echo "<script type='text/javascript'>
+        //             alert ('Invalid format for the image. Only jpg / jpeg / png format allowed.');
+        //         </script>";
+        //     $problem = true;
+        // } else {
+        //     //$game_img = md5($game_cover).$extension;
+        //     //move_uploaded_file($_FILES["gamecover"]["tmp_name"], "uploadedImg/".$game_img);
+        //     $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
+        // }
 
         if (!$problem) {
             $game_covertest = $game_cover;
