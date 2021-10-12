7f1022489925b3af620320453dddd632f17a0ce7
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-10-13 01:26:16 +0800

    Code cleanup for addGameDetails.php

--- addGameDetails.php
+++ addGameDetails.php
@@ -11,39 +11,22 @@ $game_name = mysqli_real_escape_string($conn, $_POST["gamename"]);
 $game_desc = mysqli_real_escape_string($conn, $_POST["gamedescription"]);
 $game_publisher = mysqli_real_escape_string($conn, $_POST["gamepublisher"]);
 $game_cover = $_FILES["gamecover"]["name"];
-
 $game_year = $_POST["gameyear"];
 
         $problem = false;
         $use = $_SESSION['username'];
 
-        // $extension = substr($game_cover, strlen($game_cover)-4, strlen($game_cover));
-        // $allowed_extensions = array(".jpg", ".png", ".jpeg");
-        // if (!in_array($extension, $allowed_extensions)) {
-        //     echo "<script type='text/javascript'>
-        //             alert ('Invalid format for the image. Only jpg / jpeg / png format allowed.');
-        //         </script>";
-        //     $problem = true;
-        // } else {
-        //     //$game_img = md5($game_cover).$extension;
-        //     //move_uploaded_file($_FILES["gamecover"]["tmp_name"], "uploadedImg/".$game_img);
-        //     $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
-        // }
-
         if (!$problem) {
-            $game_covertest = $game_cover;
-            $extension = substr($game_covertest, strlen($game_covertest)-4, strlen($game_covertest));
+            $game_covercontent = $game_covercontent;
+            $extension = substr($game_covercontent, strlen($game_covercontent)-4, strlen($game_covercontent));
             $allowed_extensions = array(".jpg", ".png", ".jpeg");
             $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
-
             if (!in_array($extension, $allowed_extensions)) {
                 echo "<script type='text/javascript'>
                     alert ('Invalid format for the image. Only jpg / jpeg / png format allowed.');
                 </script>";
                 $problem = true;
             } else {
-                //$game_img = md5($game_cover).$extension;
-                //move_uploaded_file($_FILES["gamecover"]["tmp_name"], "uploadedImg/".$game_img);
                 $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
             }
 
