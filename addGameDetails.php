256a14b3941e06f3b53ede0d72587455742a49bb
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-10-12 23:39:46 +0800

    Adding function to addGameDetails

--- addGameDetails.php
+++ addGameDetails.php
@@ -1,24 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+  error_reporting(0);
+  session_start();
 
-<head>
-    <title>Add Game Details</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+$conn = mysqli_connect("localhost", "root", "", "GameReviewWebsite");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL.";
+}
 
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <?php include 'header.php';
-    ?>
-    <style>
-    </style>
-</head>
+$game_name = mysqli_real_escape_string($conn, $_POST["gamename"]);
+$game_desc = mysqli_real_escape_string($conn, $_POST["gamedescription"]);
+$game_publisher = mysqli_real_escape_string($conn, $_POST["gamepublisher"]);
+$game_cover = $_FILES["gamecover"]["name"];
+$game_year = $_POST["gameyear"];
 
-<body>
+addGame($game_name, $game_desc, $game_publisher, $game_cover, $game_year);
 
+function addGame($game_name, $game_desc, $game_publisher, $game_cover, $game_year)
+{
+    if (isset($_POST['add_game'])) {
+        $conn = mysqli_connect("localhost", "root", "", "GameReviewWebsite");
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL.";
+        }
+    
+        $problem = false;
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+        //if ($_SERVER["REQUEST METHOD"] == "POST") {
+        if (!preg_match('/^[a-zA-Z0-9_]+$/', trim($game_name))) {
+            echo "<script type='text/javascript'>
+                    alert ('Game name can only contain letters, numbers, and underscores.');
+                </script>";
+                 
+            $problem = true;
+        } else {
+            $name = $game_name;
+        }
+
+        if (!preg_match('/^[a-zA-Z0-9_]+$/', trim($game_publisher))) {
+            echo "<script type='text/javascript'>
+                    alert ('Game publisher can only contain letters, numbers, and underscores.');
+                </script>";
+            $problem = true;
+        } else {
+            $publisher = $game_publisher;
+        }
+            
+        if (!preg_match('/^[a-zA-Z0-9_]+$/', trim($game_desc))) {
+            echo "<script type='text/javascript'>
+                    alert ('Game description can only contain letters, numbers, and underscores.');
+                </script>";
+            $problem = true;
+        } else {
+            $description = $game_desc;
+        }
+
+        $extension = substr($game_cover, strlen($game_cover)-4, strlen($game_cover));
+        $allowed_extensions = array(".jpg", ".png", ".jpeg");
+        if (!in_array($extension, $allowed_extensions)) {
+            echo "<script type='text/javascript'>
+                    alert ('Invalid format for the image. Only jpg / jpeg / png format allowed.');
+                </script>";
+            $problem = true;
+        } else {
+            $game_img = md5($game_cover).$extension;
+            move_uploaded_file($_FILES["gamecover"]["tmp_name"], "uploadedImg/".$game_img);
+        }
+
+        if (!$problem) {
+            date_default_timezone_set("Asia/Kuala_Lumpur");
+            $game_datetime = date('Y-m-d H:i:s');
+            $sql = "INSERT INTO games ( user_id, game_name, game_desc, game_publisher, game_datetime, game_year, game_cover)
+                        VALUES ((SELECT user_id from users WHERE username= {$_SESSION['username']}), $name,
+                        $description, $publisher, $game_datetime, $game_year, $game_img)";
+            if (mysqli_query($conn, $sql)) {
+                echo "<script type='text/javascript'>
+                        alert ('Game added successfully!');
+                        </script>";
+                header("Location: GameBrowsingHomepage.php");
+                exit();
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            echo "testmessage";
+        }
+        //if}
+    }
+}
