466a2df73d95ea5f7cca9a7b15502c33aa159e1a
annieooijiayi <70126922+annieooijiayi@users.noreply.github.com>
2021-10-18 21:38:30 +0800

    Create GameRepository.php

---
+++ src/GameRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace TDD\AddGameReview;
+
+class GameRepository
+{
+    protected $pdo;
+
+    protected function getPdo(): \PDO
+    {
+        if ($this->pdo === null)
+        {
+            $options = [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            ];
+
+            try{
+                $this -> pdo = new  \PDO("mysql:host=localhost; dbname=gamereviewwebsite;charset=utf8mb4", 'root', '', $options);
+            }catch(\PDOException $PDOException){
+
+                throw new \PDOException($PDOException->getMessage(), (int) $PDOException->getCode());
+            }
+        }
+        return $this ->pdo;
+    }
+
+    /**
+     *Fetch an array of games from the database
+     *
+     * @return array
+     */
+    public function fetchGames(): array
+    {
+        return $this->getPdo()->prepare('SELECT * FROM games')->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+
+}
