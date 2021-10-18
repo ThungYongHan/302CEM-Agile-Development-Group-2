20c9108363dbd49441ebca5a3613111db58acc79
annieooijiayi <70126922+annieooijiayi@users.noreply.github.com>
2021-10-18 21:40:22 +0800

    Update autoload_real.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit218d80ce2b6cc1ba921d59701ed00199
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit218d80ce2b6cc1ba921d59701ed00199::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire218d80ce2b6cc1ba921d59701ed00199($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire218d80ce2b6cc1ba921d59701ed00199($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
