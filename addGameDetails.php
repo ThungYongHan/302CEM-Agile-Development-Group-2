59cfde2ee7ef30b674a842a9b03a2fdf529be23b
HappyJon-web <jonathanang4978@gmail.com>
2021-10-21 17:07:05 +0800

    Update addGameDetails.php

--- addGameDetails.php
+++ addGameDetails.php
@@ -1,6 +1,6 @@
 <?php
-  error_reporting(0);
-  session_start();
+error_reporting(0);
+session_start();
 
 $conn = mysqli_connect("localhost", "root", "", "GameReviewWebsite");
 if (mysqli_connect_errno()) {
@@ -8,53 +8,86 @@ if (mysqli_connect_errno()) {
 }
 
 $problem = false;
+date_default_timezone_set("Asia/Kuala_Lumpur");
 
+$user = $_SESSION['username'];
 $game_name = mysqli_real_escape_string($conn, $_POST["gamename"]);
 $game_desc = mysqli_real_escape_string($conn, $_POST["gamedescription"]);
 $game_publisher = mysqli_real_escape_string($conn, $_POST["gamepublisher"]);
+$game_datetime = date('Y-m-d H:i:s');
 $game_year = $_POST["gameyear"];
-$user = $_SESSION['username'];
+$game_cover = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
 
-addGame($game_name, $game_desc, $game_publisher, $game_year, $user);
+addGame($user, $game_name, $game_desc, $game_publisher, $game_datetime, $game_year, $game_cover);
 
-function addGame($game_name, $game_desc, $game_publisher, $game_year, $user)
+function addGame($user, $game_name, $game_desc, $game_publisher, $game_datetime, $game_year, $game_cover)
 {
     $conn = mysqli_connect("localhost", "root", "", "GameReviewWebsite");
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL.";
     }
-    $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
+
+    $sql1 = "SELECT game_id FROM games WHERE game_name = ?";
+
+    if ($statement = mysqli_prepare($conn, $sql1)) {
+        mysqli_stmt_bind_param($statement, "s", $param_gamename);
+
+        $param_gamename = trim($game_name);
+
+        if (mysqli_stmt_execute($statement)) {
+            mysqli_stmt_store_result($statement);
+
+            if (mysqli_stmt_num_rows($statement) == 1) {
+                echo "<script type='text/javascript'>
+                    alert ('The game is already inserted.');
+                    window.location.href='GameBrowsingHomepage.php';
+                </script>";
+
+                $problem = true;
+            } else {
+                $game_name = trim($game_name);
+            }
+        }
+        mysqli_stmt_close($statement);
+    }
+
     if ($_FILES["gamecover"]["size"] > 60000) {
         fileSizeAlert();
         $problem = true;
     } else {
-        $game_img = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
+        $game_cover = addslashes(file_get_contents($_FILES["gamecover"]["tmp_name"]));
     }
     if (!$problem) {
-        date_default_timezone_set("Asia/Kuala_Lumpur");
-        $game_datetime = date('Y-m-d H:i:s');
         $sql = "INSERT INTO games (user_id, game_name, game_desc, game_publisher, game_datetime, game_year, game_cover)
                         VALUES ((SELECT user_id FROM users WHERE username= '$user'), '$game_name', '$game_desc', 
-                        '$game_publisher', '$game_datetime' , '$game_year', '$game_img')";
+                        '$game_publisher', '$game_datetime' , '$game_year', '$game_cover')";
 
         if (mysqli_query($conn, $sql)) {
-            echo "<script type='text/javascript'>
-                        alert ('Game added successfully!');
-                        </script>";
-            header("Location: GameBrowsingHomepage.php");
+            AddSuccessAlert();
             exit();
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }
 }
-          
+
 function fileSizeAlert()
 {
     echo
     "
     <script>
         window.alert('Uploaded game cover image file size is over the 60kb limit.');
+        window.location.href='GameBrowsingHomepage.php';
+    </script>
+    ";
+}
+
+function AddSuccessAlert()
+{
+    echo
+    "
+    <script>
+        window.alert('Game added successfully.');
         window.location.href='GameBrowsingHomepage.php';
     </script>
     ";
