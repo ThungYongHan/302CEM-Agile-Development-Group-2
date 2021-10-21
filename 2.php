53c40a9c1d6b8ab38fbb3a59a8ba1a530f65416e
HappyJon-web <jonathanang4978@gmail.com>
2021-10-21 17:06:52 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,6 +1,14 @@
 <?php
 $title = 'League of Legends';
-include ("header.php");
+error_reporting(0);
+session_start();
+$title = "Game Browsing Homepage";
+$user = $_SESSION['username'];
+if (empty($user)) {
+    include('header.php');
+} else {
+    include('loggedinheader.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -85,6 +93,22 @@ include ("header.php");
     .review-list {
       padding-left: 25px;
     }
+
+    .article-bg{
+        background-color: #5979f8;
+        border: none;
+        border-radius: 5px;
+    }
+
+    .divider{
+        border-bottom-width: 2px;
+        border-bottom-style: dotted;
+    }
+
+    .reviews{
+        padding-top:0.5em;
+    }
+
   </style>
 </head>
 
@@ -110,7 +134,6 @@ include ("header.php");
       $result=mysqli_query($link,$sql);
       // $resultCheck = mysqli_num_rows($result);
 
-      
         while($row =mysqli_fetch_array($result)){
           echo '<div class="GameTitleYear">';
            echo '<h2>'.$row['game_name'].'</h2>';
@@ -146,7 +169,7 @@ include ("header.php");
  
   </section>
 
-  <section id="game-review-container">
+  <article id="game-review-container">
       <div class="review-list">
       <h4>Ratings and Reviews</h4>
         <hr />
@@ -170,29 +193,29 @@ LEFT JOIN users ON reviews.user_id = users.user_id
 WHERE game_id=2";
 if($result = mysqli_query($link, $sql)){
     if(mysqli_num_rows($result) > 0){
-     
-        echo '<table class="table text-light">'; 
-        echo '<thead>';
-            echo "<tr>";
-               
-                echo "<th>Username</th>";
-                echo "<th>Review</th>";
-                echo "<th>Ratings</th>";
-                echo "<th>Date posted</th>";
-            echo "</tr>";
-            echo '</thead>';
+
         while($row = mysqli_fetch_array($result)){
-          echo '<tbody>';
-            echo "<tr>";
             
-                echo "<td>" . $row['username'] . "</td>";
-                echo "<td>" . $row['user_review'] . "</td>";
-                echo "<td>" . $row['review_num'] . "</td>";
-                echo "<td>" .date('d/m/Y', strtotime($row['review_datetime'])). "</td>";
-            echo "</tr>";
-            echo '</tbody>';
+          
+          
+          
+          print '<div class="container-fluid">
+                <div class="row g-1">
+                
+                <p class="text-light fw-bold fs-4">'.$row['username'].' <span class="fs-6"> on</span> <span class="fs-6" style="color:#5979f8"> '.date('d-m-Y',strtotime($row['review_datetime'])).'</span></p>';
+
+            print '<div class="article-bg mx-auto p-3"> <span class="fs-6 badge bg-dark">';
+            print $row['review_num']. ' <i style="color:#c9d3fc;" class="fa fa-star"></i></span><div class="divider p-1"></div>';
+
+            print '<div class="reviews"><p class="fs-5">'.$row['user_review']."</div>";
+
+            print '</div></div></div><br>';
+
         }
-        echo "</table>";
+
+
+
+
         // Free result set
         mysqli_free_result($result);
     } else{
@@ -202,13 +225,17 @@ if($result = mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
+
+
+
 // Close connection
 mysqli_close($link);
 ?>
       </div>
-      <fieldset disabled>
+      <fieldset >
         <div class="review-container">
           <h4>Rate and write a review about the game</h4>
+            <hr>
           <div class="rating-slider">
             <label for="game-rating">Rate the game</label>
             <br />
@@ -228,7 +255,7 @@ mysqli_close($link);
         </div>
       </fieldset>
    
-  </section>
+</article>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
