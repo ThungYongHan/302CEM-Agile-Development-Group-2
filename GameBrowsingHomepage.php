e03b5d0231ff32543883cb25a89ce4f5244d8983
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-10-25 15:41:00 +0800

    Update GameBrowsingHomepage.php

--- GameBrowsingHomepage.php
+++ GameBrowsingHomepage.php
@@ -155,4 +155,11 @@ if (empty($user)) {
 </section>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
 </html>
+
+
+
+
