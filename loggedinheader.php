46050f6cf7921c40e41b443b866cf992dfdaf8cc
Thung Yong Han <64062732+ThungYongHan@users.noreply.github.com>
2021-11-11 19:55:04 +0800

    Update loggedinheader.php for admin redirect

--- loggedinheader.php
+++ loggedinheader.php
@@ -1,9 +1,9 @@
 <?php
-    if (isset($_GET['logout'])) {
-        session_destroy();
-        unset($_SESSION['username']);
-        header('Location: http://localhost:8080/302CEM-Agile-Development-Group-2-master/GameBrowsingHomepage.php');
-    }
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location: http://localhost:8080/302CEM-Agile-Development-Group-2-master/GameBrowsingHomepage.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,35 +19,42 @@
     <title>Game Browsing Homepage</title>
 </head>
 <body>
-    
+
 <script>
-function LogoutFunction() {
-    if (confirm('Are you sure you want to log out?'))window.location.href="GameBrowsingHomepage.php?logout='1'";
-}
+    function LogoutFunction() {
+        if (confirm('Are you sure you want to log out?'))window.location.href="GameBrowsingHomepage.php?logout='1'";
+    }
 </script>
 
-    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #c0d3fc;">
-        <div class="container">
-          <a href="GameBrowsingHomepage.php" a class="navbar-brand text-dark">Game Review Website</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #c0d3fc;">
+    <div class="container">
+        <?php
+        if (($_SESSION['username']) == 'admin'){
+            echo "<a href=\"GameBrowsingHomepage_Admin.php\" a class=\"navbar-brand text-dark\">Game Review Website</a>";
+        }
+        else{
+            echo "<a href=\"GameBrowsingHomepage.php\" a class=\"navbar-brand text-dark\">Game Review Website</a>";
+        }
+        ?>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
             <form class="container d-flex">
                 <div class="input-group">
                   <span class="input-group-text btn btn-secondary" id="search">
                       <i class="fa fa-search"></i>
                     </span>
-                  <input type="text" class="form-control me-3" placeholder="Search for games..." aria-label="Games" aria-describedby="search-func">
+                    <input type="text" class="form-control me-3" placeholder="Search for games..." aria-label="Games" aria-describedby="search-func">
                 </div>
-                   <?php if (isset($_SESSION['username'])) : ?>
+                <?php if (isset($_SESSION['username'])) : ?>
                     <a class="navbar-brand text-dark">Welcome, <?php echo $_SESSION['username']; ?></a>
-                     <button class="btn btn-secondary" button type="button" onclick="LogoutFunction()">LogOut</button>
-                  <?php endif ?>
+                    <button class="btn btn-secondary" button type="button" onclick="LogoutFunction()">LogOut</button>
+                <?php endif ?>
             </form>
-          </div>
         </div>
-      </nav>
+    </div>
+</nav>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
