fa4dfe8283e23285db9503135082d41611c8c6fd
HappyJon-web <jonathanang4978@gmail.com>
2021-11-11 21:42:08 +0800

    Update createTable.php

--- createTable.php
+++ createTable.php
@@ -10,6 +10,7 @@ username VARCHAR(256) NOT NULL,
 user_email VARCHAR(256) NOT NULL,
 user_pass VARCHAR(256) NOT NULL,
 vkey VARCHAR(50) NOT NULL,
+user_role VARCHAR(256) NOT NULL,
 status VARCHAR(10) NOT NULL DEFAULT '0',
 PRIMARY KEY (user_id)
 )";
@@ -24,7 +25,7 @@ game_datetime DATETIME NOT NULL,
 game_year YEAR NOT NULL,
 game_cover BLOB NOT NULL,
 PRIMARY KEY (game_id),
-CONSTRAINT FK_gameuser_id FOREIGN KEY (user_id) REFERENCES Users(user_id)
+CONSTRAINT FK_gameuser_id FOREIGN KEY (user_id) REFERENCES Users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
 )";
 
 $sqlReviews = "CREATE TABLE IF NOT EXISTS Reviews (
@@ -35,21 +36,22 @@ user_review VARCHAR(256) NOT NULL,
 review_num INT(6) NOT NULL,
 review_datetime DATETIME NOT NULL,
 PRIMARY KEY (review_id),
-CONSTRAINT FK_reviewuser_id FOREIGN KEY (user_id) REFERENCES Users(user_id),
-CONSTRAINT FK_reviewgame_id FOREIGN KEY (game_id) REFERENCES Games(game_id)
+CONSTRAINT FK_reviewuser_id FOREIGN KEY (user_id) REFERENCES Users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+CONSTRAINT FK_reviewgame_id FOREIGN KEY (game_id) REFERENCES Games(game_id) ON UPDATE CASCADE ON DELETE CASCADE
 )";
 
-$sqlInsertUsers = "INSERT INTO Users (username, user_email, user_pass) VALUES 
-('user1', 'user1@example.com', 'p455w0rd'),
-('user2', 'user2@example.com', 'p455w0rd'),
-('user3', 'user3@example.com', 'p455w0rd')";
+$sqlInsertUsers = "INSERT INTO Users (username, user_email, user_pass, user_role, status) VALUES 
+('user1', 'user1@example.com', 'p455w0rd', 'user', 'Verified'),
+('user2', 'user2@example.com', 'p455w0rd', 'user', 'Verified'),
+('user3', 'user3@example.com', 'p455w0rd', 'user', 'Verified'),
+('admin', 'admin@example.com', 'p455w0rd', 'admin', 'Verified')";
 
 $sqlInsertGames = "INSERT INTO Games (user_id, game_name, game_desc, game_publisher, game_datetime, game_year, game_cover) VALUES 
-((SELECT user_id FROM Users WHERE username ='user1'), 'Genshin Impact', 'Genshin Impact is an open-world action role-playing game that allows the player to control one of four characters in a party. Switching between characters can be done quickly during combat, allowing the player to use different combinations of skills.', 'miHoYo', '2021-09-01 09:00:00', '2017', LOAD_FILE('C:/xampp/htdocs/302CEM-Agile-Development-Group-2-master/images/Genshin_cover.jpg')),
-((SELECT user_id FROM Users WHERE username='user2'), 'League of Legends','League of Legends features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.','Riot Games', '2021-08-01 08:00:00', '2009', LOAD_FILE('C:/xampp/htdocs/302CEM-Agile-Development-Group-2-master/images/League_cover.jpg')),
-((SELECT user_id FROM Users WHERE username='user3'), 'Fast & Furious Crossroads', 'Fast & Furious Crossroads is a racing and action role-playing video game based on the Fast & Furious film franchise.  Gear up for an epic new chapter in the Fast & Furious saga with high-speed heists, cinematic nonstop action, and adrenaline-fueled stunts.','Bandai Namco', '2021-07-01 07:00:00', '2020', LOAD_FILE('C:/xampp/htdocs/302CEM-Agile-Development-Group-2-master/images/Furious_cover.jpg')),
-((SELECT user_id FROM Users WHERE username='user1'), 'Minecraft', 'Explore infinite worlds and build everything from the simplest of homes to the grandest of castles in Minecraft. Play in creative mode with unlimited resources or mine deep into the world in survival mode, crafting weapons to fend off mobs.','Mojang Studios', '2021-06-01 06:00:00', '2011', LOAD_FILE('C:/xampp/htdocs/302CEM-Agile-Development-Group-2-master/images/Minecraft_cover.jpeg')),
-((SELECT user_id FROM Users WHERE username='user2'), 'Dawn of Fear', 'Dawn of Fear is a survival horror with 3rd person camera that tells the story of Alex, a young man with a past marked by tragedy. The player will have to solve puzzles, collect clues and objects, and survive.','Brok3nsite', '2021-05-01 05:00:00', '2020', LOAD_FILE('C:/xampp/htdocs/302CEM-Agile-Development-Group-2-master/images/Dawn_cover.jpg'))";
+((SELECT user_id FROM Users WHERE username ='user1'), 'Genshin Impact', 'Genshin Impact is an open-world action role-playing game that allows the player to control one of four characters in a party. Switching between characters can be done quickly during combat, allowing the player to use different combinations of skills.', 'miHoYo', '2021-09-01 09:00:00', '2017', LOAD_FILE('C:/xampp1/htdocs/302CEM-Agile-Development-Group-2-master/images/Genshin_cover.jpg')),
+((SELECT user_id FROM Users WHERE username='user2'), 'League of Legends','League of Legends features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.','Riot Games', '2021-08-01 08:00:00', '2009', LOAD_FILE('C:/xampp1/htdocs/302CEM-Agile-Development-Group-2-master/images/League_cover.jpg')),
+((SELECT user_id FROM Users WHERE username='user3'), 'Fast & Furious Crossroads', 'Fast & Furious Crossroads is a racing and action role-playing video game based on the Fast & Furious film franchise.  Gear up for an epic new chapter in the Fast & Furious saga with high-speed heists, cinematic nonstop action, and adrenaline-fueled stunts.','Bandai Namco', '2021-07-01 07:00:00', '2020', LOAD_FILE('C:/xampp1/htdocs/302CEM-Agile-Development-Group-2-master/images/Furious_cover.jpg')),
+((SELECT user_id FROM Users WHERE username='user1'), 'Minecraft', 'Explore infinite worlds and build everything from the simplest of homes to the grandest of castles in Minecraft. Play in creative mode with unlimited resources or mine deep into the world in survival mode, crafting weapons to fend off mobs.','Mojang Studios', '2021-06-01 06:00:00', '2011', LOAD_FILE('C:/xampp1/htdocs/302CEM-Agile-Development-Group-2-master/images/Minecraft_cover.jpeg')),
+((SELECT user_id FROM Users WHERE username='user2'), 'Dawn of Fear', 'Dawn of Fear is a survival horror with 3rd person camera that tells the story of Alex, a young man with a past marked by tragedy. The player will have to solve puzzles, collect clues and objects, and survive.','Brok3nsite', '2021-05-01 05:00:00', '2020', LOAD_FILE('C:/xampp1/htdocs/302CEM-Agile-Development-Group-2-master/images/Dawn_cover.jpg'))";
 
 $sqlInsertReviews = "INSERT INTO Reviews (user_id, game_id, user_review, review_num, review_datetime) VALUES
 ((SELECT user_id FROM Users WHERE username ='user1'), (SELECT game_id FROM Games WHERE game_name ='Genshin Impact'), 'If you are looking for a fun game that you can just sit back and relax with, then Genshin Impact is excellent for that. The game’s world is pleasant to drink in and the combat is a nice balance of being involved, while not becoming overly complicated.', 5, '2021-09-01 09:00:00'),
